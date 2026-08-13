a895ca46a9fa0f0d4757695968a1ef0b59dbb438
mwiz02 <a.prasad2047@gmail.com>
2026-08-13 15:17:29 -0700

    refactor preprocessing into reusable function with main guard

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,17 +1,20 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('data/receipts/sample_receipt.jpg')
+def preprocess_image(image_path):
+    img = cv.imread(image_path)
 
-greyscale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    greyscale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-blurred_img = cv.GaussianBlur(greyscale_img, (5, 5), 0)
+    blurred_img = cv.GaussianBlur(greyscale_img, (5, 5), 0)
 
-binary_img = cv.adaptiveThreshold(blurred_img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+    binary_img = cv.adaptiveThreshold(blurred_img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
 
+    return binary_img
 
+if __name__ == "__main__":
+    preprocessed_image = preprocess_image('data/receipts/sample_receipt.jpg')
+    cv.imshow('receipt', preprocessed_image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
-
-cv.imshow('receipt', binary_img)
-cv.waitKey(0)
-cv.destroyAllWindows()
